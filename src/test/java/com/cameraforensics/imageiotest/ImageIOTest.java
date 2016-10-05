e5fdd31f49210f4082d970c6bda4f85c5bb2111b
Matt <matt@mattburns.co.uk>
2016-10-05 16:23:05 +0100

    Attempting to convert with various ICC profiles and their inverses

--- src/test/java/com/cameraforensics/imageiotest/ImageIOTest.java
+++ src/test/java/com/cameraforensics/imageiotest/ImageIOTest.java
@@ -13,7 +13,9 @@ import java.io.File;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class ImageIOTest {
 
@@ -25,27 +27,96 @@ public class ImageIOTest {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("test.jpg").getFile());
 
-        System.out.println("\nTool        : [x, y] = [  r,   g,   b]\n");
+        java.util.List<String> profileNames = new ArrayList<>();
+        profileNames.add("ACESCG Linear.icc");
+        profileNames.add("AdobeRGB1998.icc");
+        profileNames.add("Black & White.icc");
+        profileNames.add("Blue Tone.icc");
+        profileNames.add("CASIO-PJ-0443CAD9-4B4B-3516-1596-C2E9140C00D3.icc");
+        profileNames.add("CP220c-9B738A31-361D-020A-DE87-51E587421F82.icc");
+        profileNames.add("Color LCD-0AA6E386-CE2C-5EE2-CC95-F2B67DCDF861.icc");
+        profileNames.add("DCI(P3) RGB.icc");
+        profileNames.add("DELL P2415Q-C1C4C3E1-E9D8-3D14-9BCD-F6DA2A7E7B16.icc");
+        profileNames.add("DELL U2515H-A2C875F7-5EAD-E995-0965-2C3DA97AE162.icc");
+        profileNames.add("Display P3.icc");
+        profileNames.add("Display-8D79CBC9-A4EC-DDE1-12FC-91BA625B13C2.icc");
+        profileNames.add("Generic CMYK Profile.icc");
+        profileNames.add("Generic Gray Gamma 2.2 Profile.icc");
+        profileNames.add("Generic Gray Profile.icc");
+        profileNames.add("Generic Lab Profile.icc");
+        profileNames.add("Generic RGB Profile.icc");
+        profileNames.add("Generic XYZ Profile.icc");
+        profileNames.add("Gray Tone.icc");
+        profileNames.add("ITU-2020.icc");
+        profileNames.add("ITU-709.icc");
+        profileNames.add("LG SIGNAGE-F89847F9-FD21-3765-B9D2-FAD087B57067.icc");
+        profileNames.add("Lightness Decrease.icc");
+        profileNames.add("Lightness Increase.icc");
+        profileNames.add("MB169B+      -79A5DF33-C97B-34FF-E507-0E70DA3D3A21.icc");
+        profileNames.add("Optoma XGA-204B4EE6-5226-36A8-1FBD-FB3D0BA774BC.icc");
+        profileNames.add("ROMM RGB.icc");
+        profileNames.add("SX20-8C39FF0F-26AD-70C1-BE25-ABB14AACEC3B.icc");
+        profileNames.add("Sepia Tone.icc");
+        profileNames.add("default_cmyk.icc");
+        profileNames.add("default_gray.icc");
+        profileNames.add("default_rgb.icc");
+        profileNames.add("gray_to_k.icc");
+        profileNames.add("lab.icc");
+        profileNames.add("ps_cmyk.icc");
+        profileNames.add("ps_gray.icc");
+        profileNames.add("ps_rgb.icc");
+        profileNames.add("sRGB Profile.icc");
+        profileNames.add("sRGB_ICC_v4_Appearance.icc");
+        profileNames.add("sgray.icc");
+        profileNames.add("srgb.icc");
+
+        System.out.printf("%65s : [x, y] = [  r,   g,   b]\n", "tool");
+
+        List<String> profileNamesToSkip = new ArrayList<>();
+        List<String> inverseProfileNamesToSkip = new ArrayList<>();
+
         for (int y = 0; y < 3; y++) {
             for (int x = 0; x < 3; x++) {
-                int[] imageio1RGB = getRGBUsingImageIO1(file, x, y);
-                int[] imageio2RGB = getRGBUsingImageIO2(file, x, y);
+                int[] imageioRGB = getRGBUsingImageIO(file, x, y);
                 int[] pythonRGB = getRGBUsingPython(file, x, y);
                 int[] imageMagickRGB = getRGBUsingImageMagick(file, x, y);
 
-                String diff = "";
-                if (!Arrays.equals(imageio1RGB, pythonRGB)) {
-                    diff = "<--- different to python";
+                printResults("Image IO", x, y, imageioRGB, pythonRGB);
+                printResults("Python", x, y, pythonRGB, pythonRGB);
+                printResults("ImageMagick", x, y, imageMagickRGB, pythonRGB);
+
+                for (String profileName : profileNames) {
+                    if (!profileNamesToSkip.contains(profileName)) {
+                        int[] profileRGB = getRGBUsingProfile(file, x, y, profileName, false);
+                        printResults(profileName, x, y, profileRGB, pythonRGB);
+                        if (!Arrays.equals(profileRGB, pythonRGB)) {
+                            profileNamesToSkip.add(profileName);
+                        }
+                    }
+
+                    if (!inverseProfileNamesToSkip.contains(profileName)) {
+                        int[] profileInverseRGB = getRGBUsingProfile(file, x, y, profileName, true);
+                        printResults("inverse " + profileName, x, y, profileInverseRGB, pythonRGB);
+                        if (!Arrays.equals(profileInverseRGB, pythonRGB)) {
+                            inverseProfileNamesToSkip.add(profileName);
+                        }
+                    }
                 }
-                System.out.printf("Image IO 1  : [%d, %d] = %s %s\n", x, y, Arrays.toString(imageio1RGB), diff);
-                System.out.printf("Image IO 2  : [%d, %d] = %s %s\n", x, y, Arrays.toString(imageio2RGB), diff);
-                System.out.printf("Python      : [%d, %d] = %s\n", x, y, Arrays.toString(pythonRGB));
-                System.out.printf("ImageMagick : [%d, %d] = %s\n\n", x, y, Arrays.toString(imageMagickRGB));
+
+                System.out.println();
             }
         }
     }
 
-    private int[] getRGBUsingImageIO1(File file, int x, int y) throws IOException {
+    private void printResults(String tool, int x, int y, int[] rgb, int[] pythonRGB) {
+        String diff = "";
+        if (!Arrays.equals(rgb, pythonRGB)) {
+            diff = "<--- different to python";
+        }
+        System.out.printf("%65s : [%d, %d] = [%3d, %3d, %3d] %s\n", tool, x, y, rgb[0], rgb[1], rgb[2], diff);
+    }
+
+    private int[] getRGBUsingImageIO(File file, int x, int y) throws IOException {
         BufferedImage image = ImageIO.read(file);
 
         int javaRGB = image.getRGB(x, y);
@@ -56,13 +127,21 @@ public class ImageIOTest {
         return new int[]{javaRed, javaGreen, javaBlue};
     }
 
-    private ICC_Profile cp = ICC_Profile.getInstance("src/test/resources/sRGB_ICC_v4_Appearance.icc");
-    private ICC_ColorSpace cs = new ICC_ColorSpace(cp);
+    private ICC_Profile sRGBProfile = ICC_Profile.getInstance("src/test/resources/sRGB_ICC_v4_Appearance.icc");
+    private ICC_ColorSpace sRGBColorSpace = new ICC_ColorSpace(sRGBProfile);
 
-    private int[] getRGBUsingImageIO2(File file, int x, int y) throws IOException {
+    private int[] getRGBUsingProfile(File file, int x, int y, String profileName, boolean inverse) throws IOException {
+        ICC_Profile cp = ICC_Profile.getInstance("src/test/resources/" + profileName);
+        ICC_ColorSpace cs = new ICC_ColorSpace(cp);
+
         BufferedImage image = ImageIO.read(file);
-        ColorConvertOp cco = new ColorConvertOp( cs, null );
-        BufferedImage result = cco.filter( image, null );
+        ColorConvertOp cco;
+        if (inverse) {
+            cco = new ColorConvertOp(sRGBColorSpace, cs, null);
+        } else {
+            cco = new ColorConvertOp(cs, null);
+        }
+        BufferedImage result = cco.filter(image, null);
         int javaRGB = result.getRGB(x, y);
         int javaRed = (javaRGB >> 16) & 0xFF;
         int javaGreen = (javaRGB >> 8) & 0xFF;
@@ -75,7 +154,7 @@ public class ImageIOTest {
         String output = runProcess("python src/test/resources/getRGB.py " + image.getAbsolutePath() + " " + x + " " + y);
         String[] parts = output.replace("(", "").replace(")", "").replaceAll(" ", "").split(",");
         int[] rgb = new int[3];
-        for (int i = 0 ; i < 3 ; i++){
+        for (int i = 0; i < 3; i++) {
             rgb[i] = Integer.parseInt(parts[i]);
         }
         return rgb;
